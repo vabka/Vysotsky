d5634f4190be2d685fdff91ab51f4540565bc3ce
Vasiliy Bannikov <vab-999@ya.ru>
2021-04-06 12:51:39 +0500

    docs

---
+++ Vysotsky.API/Controllers/ApiController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Vysotsky.API.Controllers
+{
+    public abstract class ApiController : Controller
+    {
+        protected ActionResult<ApiResponse<T>> Success<T>(T result)
+        {
+            var apiResponse = new ApiResponse<T>
+            {
+                Status = ResponseStatus.Ok,
+                Result = result
+            };
+            return Ok(apiResponse);
+        }
+
+        protected ActionResult<ApiResponse<T>> MalformedRequest<T>(string message)
+        {
+            var apiResponse = new ApiResponse<T>()
+            {
+                Status = ResponseStatus.Error,
+                Error = new ApiError
+                {
+                    Message = message
+                }
+            };
+            return BadRequest(apiResponse);
+        }
+    }
+
+    public class ApiResponse<T>
+    {
+        public ResponseStatus Status { get; init; }
+        public ApiError? Error { get; init; }
+        public T? Result { get; init; }
+    }
+
+    public enum ResponseStatus
+    {
+        Ok,
+        Error
+    }
+
+    public class ApiError
+    {
+        public string Message { get; init; } = "";
+    }
+
+    public class Paginated<T>
+    {
+        public int Total { get; init; }
+        public int Count { get; set; }
+        public int PageSize { get; init; }
+        public int PageNumber { get; init; }
+        public bool HasMore { get; init; }
+        public string? NextPage { get; init; }
+        public IReadOnlyCollection<T> Data { get; init; }
+    }
+}
