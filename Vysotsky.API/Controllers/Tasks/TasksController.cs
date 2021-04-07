b4c32e2c96aab201c9ab3f003eaf1853da2715ec
vabka <vab-999@ya.ru>
2021-04-07 13:34:15 +0500

    Добавляем комментарии в апишку

--- Vysotsky.API/Controllers/Tasks/TasksController.cs
+++ Vysotsky.API/Controllers/Tasks/TasksController.cs
@@ -1,10 +1,36 @@
+using System;
 using Microsoft.AspNetCore.Mvc;
 using Vysotsky.API.Infrastructure;
 
 namespace Vysotsky.API.Controllers.Tasks
 {
+    /// <summary>
+    /// Контроллер заявок
+    /// </summary>
     [Route(Resources.Tasks)]
     public class TasksController : ApiController
     {
+        /// <summary>
+        /// Получить данные по заявке
+        /// </summary>
+        /// <param name="taskId"></param>
+        /// <returns></returns>
+        [HttpGet("{taskId}")]
+        public ActionResult GetTask(int taskId)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Получить комментарии по заявке
+        /// </summary>
+        /// <param name="taskId">Идентификатор заявки</param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        [HttpGet("{taskId}/comments")]
+        public ActionResult GetComments(int taskId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
