95fdee5ce04c67159306e8ac04772c1653c6d23f
vabka <vab-999@ya.ru>
2021-04-15 19:52:22 +0500

    test pre-commit hook

--- Vysotsky.API/Program.cs
+++ Vysotsky.API/Program.cs
@@ -37,32 +37,32 @@ Host.CreateDefaultBuilder(args)
     .ConfigureWebHost(builder =>
     {
         builder
-     // Defaults
-     .UseKestrel((builderContext, options) =>
-         options.Configure(builderContext.Configuration.GetSection("Kestrel"), true))
-     .ConfigureServices((hostingContext, services) =>
-     {
-         services.PostConfigure<HostFilteringOptions>(options =>
-         {
-             if (options.AllowedHosts is null or { Count: 0 })
-             {
-                 var hosts = hostingContext
-                     .Configuration["AllowedHosts"]
-                     ?.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
-                 options.AllowedHosts = hosts switch
-                 {
-                     { Length: > 0 } => hosts,
-                     _ => new[] { "*" }
-                 };
-             }
-         });
-         services.AddSingleton<IOptionsChangeTokenSource<HostFilteringOptions>>(
-             new ConfigurationChangeTokenSource<HostFilteringOptions>(hostingContext.Configuration));
-         services.AddRouting();
-     })
-     .ConfigureServices((ctx, services) => Startup.ConfigureServices(services, ctx.Configuration))
-     .Configure((_, app) => app.UseHostFiltering())
-     .Configure((ctx, app) => Startup.ConfigureWebApp(app, ctx.HostingEnvironment));
+            // Defaults
+            .UseKestrel((builderContext, options) =>
+                options.Configure(builderContext.Configuration.GetSection("Kestrel"), true))
+            .ConfigureServices((hostingContext, services) =>
+            {
+                services.PostConfigure<HostFilteringOptions>(options =>
+                {
+                    if (options.AllowedHosts is null or { Count: 0 })
+                    {
+                        var hosts = hostingContext
+                            .Configuration["AllowedHosts"]
+                            ?.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+                        options.AllowedHosts = hosts switch
+                        {
+                            { Length: > 0 } => hosts,
+                            _ => new[] { "*" }
+                        };
+                    }
+                });
+                services.AddSingleton<IOptionsChangeTokenSource<HostFilteringOptions>>(
+                    new ConfigurationChangeTokenSource<HostFilteringOptions>(hostingContext.Configuration));
+                services.AddRouting();
+            })
+            .ConfigureServices((ctx, services) => Startup.ConfigureServices(services, ctx.Configuration))
+            .Configure((_, app) => app.UseHostFiltering())
+            .Configure((ctx, app) => Startup.ConfigureWebApp(app, ctx.HostingEnvironment));
     })
     .Build()
     .Run();
